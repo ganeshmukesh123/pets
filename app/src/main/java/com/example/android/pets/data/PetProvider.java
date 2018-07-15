72db6424d551d7d3b552b7adfc10d2c42a0b19d5
Ganesh D Choudhary <ganeshmukesh123@gmail.com>
2018-07-16 00:01:42 +0530

    validating data entered by user and implementing content provider  update() method

--- app/src/main/java/com/example/android/pets/data/PetProvider.java
+++ app/src/main/java/com/example/android/pets/data/PetProvider.java
@@ -88,6 +88,24 @@ public class PetProvider extends ContentProvider {
     }
 
     private Uri insertPets(Uri uri ,ContentValues contentValues){
+
+        String name = contentValues.getAsString(PetContract.PetEntry.COLUMN_PET_NAME);
+        if (name == null) {
+            throw new IllegalArgumentException("Pet requires a name");
+        }
+
+        Integer gender = contentValues.getAsInteger(PetContract.PetEntry.COLUMN_PET_GENDER);
+        if (gender == null || !PetContract.PetEntry.isValidGender(gender)) {
+            throw new IllegalArgumentException("Pet requires valid gender");
+        }
+
+        Integer weight = contentValues.getAsInteger(PetContract.PetEntry.COLUMN_PET_WEIGHT);
+        if(weight == null && weight<0){
+            throw new IllegalArgumentException(("Pet requires valid weight"));
+        }
+
+
+
         SQLiteDatabase db = mDbHelper.getWritableDatabase();
 
         long id = db.insert(PetContract.PetEntry.TABLE_NAME,null,contentValues);
@@ -106,7 +124,53 @@ public class PetProvider extends ContentProvider {
      */
     @Override
     public int update(Uri uri, ContentValues contentValues, String selection, String[] selectionArgs) {
-        return 0;
+
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PETS:
+                return updatePet(uri, contentValues, selection, selectionArgs);
+            case PET_ID:
+                // For the PET_ID code, extract out the ID from the URI,
+                // so we know which row to update. Selection will be "_id=?" and selection
+                // arguments will be a String array containing the actual ID.
+                selection = PetContract.PetEntry._ID + "=?";
+                selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri)) };
+                return updatePet(uri, contentValues, selection, selectionArgs);
+            default:
+                throw new IllegalArgumentException("Update is not supported for " + uri);
+        }
+
+    }
+
+    private int updatePet(Uri uri, ContentValues contentValues, String selection, String[] selectionArgs){
+        if (contentValues.containsKey(PetContract.PetEntry.COLUMN_PET_NAME)){
+            String name = contentValues.getAsString(PetContract.PetEntry.COLUMN_PET_NAME);
+            if (name == null){
+                throw new IllegalArgumentException("Pet requires a name");
+            }
+        }
+
+        if (contentValues.containsKey(PetContract.PetEntry.COLUMN_PET_GENDER)){
+            Integer gender = contentValues.getAsInteger(PetContract.PetEntry.COLUMN_PET_GENDER);
+            if (gender == null || !PetContract.PetEntry.isValidGender(gender)) {
+                throw new IllegalArgumentException("Pet requires valid gender");
+            }
+        }
+
+        if (contentValues.containsKey(PetContract.PetEntry.COLUMN_PET_WEIGHT)){
+            Integer weight = contentValues.getAsInteger(PetContract.PetEntry.COLUMN_PET_WEIGHT);
+            if(weight == null && weight<0){
+                throw new IllegalArgumentException(("Pet requires valid weight"));
+            }
+        }
+
+        if (contentValues.size() == 0){
+            return 0;
+        }
+
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        return db.update(PetContract.PetEntry.TABLE_NAME,contentValues,selection,selectionArgs);
     }
 
     /**
